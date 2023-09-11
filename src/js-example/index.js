fe2cd101be7cb227b9ab759e9f115e1332df4f69
Frank Gutierrez <dearfrankg@gmail.com>
2023-09-10 18:31:36 -0700

    add eslint/prettier/husky/lint-staged

--- src/js-example/index.js
+++ src/js-example/index.js
@@ -1,6 +1,9 @@
-console.log("javascript example");
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
 
-import { sum, alpha, beta } from "./sum";
+console.log('javascript example');
+
+import { sum, alpha, beta } from './sum';
 
 console.log(sum(1, 2));
 console.log(alpha(1, 2));
--- src/ts-example/index.ts
+++ src/ts-example/index.ts
@@ -1,6 +1,9 @@
-console.log("typescript example");
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
 
-import { sum, alpha, beta } from "./sum";
+console.log('typescript example');
+
+import { sum, alpha, beta } from './sum';
 
 console.log(sum(1, 2));
 console.log(alpha(1, 2));
